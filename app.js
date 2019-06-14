631c8c991a38c963f4b6448ddd7d232725309116
NikosAthanasiadis <nikosathanasiadis1990@hotmail.com>
2019-06-14 23:22:34 +0300

    First commit

---
+++ app.js
@@ -0,0 +1,61 @@
+var bodyParser = require("body-parser"),
+    mongoose = require("mongoose"),
+    express = require("express"),
+    app = express();
+
+
+mongoose.connect("mongodb://localhost:27017/restful_blog_app", {
+    useNewUrlParser: true
+});
+
+// APP CONFIG
+app.set("view engine", "ejs");
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+
+var blogSchema = new mongoose.Schema({
+    title: String,
+    image: String,
+    body: String,
+    created: {
+        type: Date,
+        default: Date.now
+    }
+});
+
+var Blog = mongoose.model("Blog", blogSchema);
+
+Blog.create({
+    title: "Test Blog",
+    image: "https://images.unsplash.com/photo-1504208434309-cb69f4fe52b0?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60",
+    body: "HELLO THIS IS A BLOG POST!"
+});
+
+// RESTFUL ROUTES 
+
+app.get("/", function (req, res) {
+    res.redirect("/blogs");
+});
+
+app.get("/blogs", function (req, res) {
+    Blog.find({}, function (err, blogs) {
+        if (err) {
+            console.log("ERROR!");
+        } else {
+            res.render("index", {blogs: blogs});
+        }
+    });
+});
+
+
+// title
+// image
+// body
+// created
+
+
+app.listen(3000, function () {
+    console.log("SERVER IS RUNNING!");
+});
