e39612f92158a4d35d07900ef03e780a177c589e
NikosAthanasiadis <nikosathanasiadis1990@hotmail.com>
2019-06-15 19:16:19 +0300

    Added The UPDATE Route
    I had to install a new npm called "methodOverride" in order to continue using restful routes.

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
-var bodyParser = require("body-parser"),
+var methodOverride = require("method-override"),
+    bodyParser = require("body-parser"),
     mongoose = require("mongoose"),
     express = require("express"),
     app = express();
@@ -8,12 +9,14 @@ mongoose.connect("mongodb://localhost:27017/restful_blog_app", {
     useNewUrlParser: true
 });
 
+mongoose.set('useFindAndModify', false);
+
+
 // APP CONFIG
 app.set("view engine", "ejs");
 app.use(express.static("public"));
-app.use(bodyParser.urlencoded({
-    extended: true
-}));
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(methodOverride("_method"));
 
 var blogSchema = new mongoose.Schema({
     title: String,
@@ -79,6 +82,27 @@ app.get("/blogs/:id", function(req, res){
             res.redirect("/blogs");
         } else {
             res.render("show", {blog: foundBlog});
+        }
+    });
+
+});//EDIT ROUTE
+app.get("/blogs/:id/edit", function(req, res){
+    Blog.findById(req.params.id, function(err, foundBlog){
+        if (err) {
+            res.redirect("/blogs");
+        } else {
+            res.render("edit", {blog: foundBlog});
+        }
+    });
+});
+
+// UPDATE ROUTE
+app.put("/blogs/:id", function(req, res){
+    Blog.findByIdAndUpdate(req.params.id, req.body.blog, function(err, updatedBlog){
+        if (err) {
+            res.redirect("/blogs");
+        } else {
+            res.redirect("/blogs/"+req.params.id);
         }
     });
 });
